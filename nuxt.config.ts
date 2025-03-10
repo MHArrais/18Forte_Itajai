dca061198ed8aa75ba9d5f7eaffa2640497e9322
MHArrais <drake_i@hotmail.com>
2025-03-10 20:16:40 -0300

    Fix: Disable Devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: [
     '@/assets/css/global.css', // Caminho para o arquivo CSS
   ],
